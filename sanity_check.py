e3df42f7535679ca78d62db77c65bffc011bdbe2
Annan Wang <annan.wang@shopee.com>
2022-06-18 18:05:48 +0800

    .

---
+++ sanity_check.py
@@ -0,0 +1,46 @@
+import os
+from PIL import Image
+
+def sanity_check(data_path, img_type):
+    train_file_name = img_type + "_train_kfold.txt"
+    val_file_name = img_type + "_crossval_kfold.txt"
+    test_file_name = img_type + "_test_kfold.txt"
+    train_set_path = os.join(data_path, "splits", train_file_name)
+    val_set_path = os.join(data_path, "splits", val_file_name)
+    test_set_path = os.join(data_path, "splits", test_file_name)
+    train_file = open(train_set_path, "r")
+    train_list = train_file.readlines()
+    val_file = open(val_set_path, "r")
+    val_list = val_file.readlines()
+    test_file = open(test_set_path, "r")
+    test_list = test_file.readlines()
+
+    train_set = {}
+    val_set = {}
+    test_set = {}
+    for l in train_list:
+        img_name, _ = l.split(' ')
+        img = Image.open(os.join(data_path, "images", img_name))
+        img.verify()
+        train_set.add(img)
+
+    for l in val_list:
+        img_name, _ = l.split(' ')
+        img = Image.open(os.join(data_path, "images", img_name))
+        img.verify()
+        val_set.add(img)
+
+    for l in test_list:
+        img_name, _ = l.split(' ')
+        img = Image.open(os.join(data_path, "images", img_name))
+        img.verify()
+        test_set.add(img)
+
+    for i in test_set:
+        if i in train_set:
+            raise RuntimeError("split_sanity_check: %s in both test and train set" % i)
+    for i in test_set:
+        if i in val_set:
+            raise RuntimeError("split_sanity_check: %s in both test and val set" % i)
+
+    print("Sanity check passed.")
