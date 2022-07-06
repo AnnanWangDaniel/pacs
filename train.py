17f482a49b41d272d98d5c3de2208390f65454bc
Annan Wang <annan.wang@shopee.com>
2022-07-06 16:47:25 +0800

    .

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ from torchvision import transforms
 from PIL import Image
 from tqdm import tqdm
 
-from models.models import *
+#from models.models import *
 from utils.utils import *
 
 DEVICE = 'cuda'      # 'cuda' or 'cpu'
